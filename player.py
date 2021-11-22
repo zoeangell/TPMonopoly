af2a7ee30b737b25b20748aa0d29b633c3b2f202
zoeangell <angellzm2003@gmail.com>
2021-11-21 21:48:36 -0500

    Update player.py

--- player.py
+++ player.py
@@ -11,6 +11,7 @@ class Player(object):
         self.color = color
         
     def curBlock(self):
+        #return curBlock as an int
         x, y = self.position
         print("x: ", x, "y: ", y)
         curblock = 0
@@ -28,6 +29,12 @@ class Player(object):
         #newblock = (curblock + distance)%totalBlocks
         #return newblock
         #return (curblock, newblock)
+    
+    def getCurBlock(self, blockNum):
+        row = blockNum // len(self.board[0])
+        col = blockNum % len(self.board[0])
+        return self.board[row][col]
+
 
     def roll(self):
         toss1 = random.randint(1,6)
@@ -39,22 +46,64 @@ class Player(object):
 
     def buyBlock(self, block):
         #Allows player to buy land
+        self.bankaccount += block.price #plus because I made the prices -
+        block.ownership = self.name
+        self.land.append(block)
+
+    def canBuy(self, block):
         if (isinstance(block, SpecialCards) or isinstance(block, Tax) or
-            isinstance(block, Corner)): return None
-        else:
-            self.bankaccount += block.price #plus because I made the prices -
-            block.ownership = self.name
-            self.land.append(block)
+            isinstance(block, Corner)): return False
+        return True
     
     def payRent(self, block):
+        #Pays the rent for a owned block
         self.bankaccount += block.rent()
 
     def isOwned(self, other, block):
         #Checks if the someone owns the block. If None then no rent and
         #it can be bought.
         if block.ownership == self.name: return True
-        elif other.ownership == other.name: return False
+        elif block.ownership == other.name: return False
         return None
+
+    def payTax(self, block):
+        self.bankaccount += block.rent()
+
+    def payIncomeTax(self):
+        #Pay 10% of your income
+        self.bankaccount *= 0.9
+
+    def payUtilityTax(self, block, roll, other):
+        #Utilities have special rules for tax. This function handles that.
+        count, tax = self.countUtilities(other), 0
+        if count == 1:
+            tax = 4*roll
+        elif count == 2:
+            tax = 10*roll
+        self.bankaccount -= tax
+        return tax
+
+    def payRailroadTax(self, block, other):
+        count, tax = self.countRailroads(other), 0
+        if count == 1: tax = 25
+        elif count == 2: tax = 50
+        elif count == 3: tax = 100
+        elif count == 4: tax = 200
+        self.bankaccount -= tax
+        return tax
+
+
+    def countUtilities(self, other):
+        count = 0
+        for block in other.land:
+            if isinstance(block, Utility): count +=1
+        return count
+
+    def countRailroads(self, other):
+        count = 0
+        for block in other.land:
+            if isinstance(block, Railroad): count +=1
+        return count
 
 
     
