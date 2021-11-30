f2803b57cc50348dd7f5491929a2f52290934092
zoeangell <angellzm2003@gmail.com>
2021-11-30 12:35:30 -0500

    Update block.py

--- block.py
+++ block.py
@@ -31,51 +31,7 @@ class SpecialCards(Block):
         super().__init__(location, color, name, price)
         self.property = None
         self.ownership = None
-        self.chance = []
-        self.fillChanceDeck()
-        self.cChest = []
-        self.fillcChestDeck()
-
-    def fillcChestDeck(self):
-        card1 = Card("Bank error in your favor. Collect $200", 200)
-        card2 = Card("Doctor's fee. Pay $50", -50)
-        card3 = Card("From sale from stock you get $50", 50)
-        card4 = Card("Holiday fund matures. Receive $100.", 100)
-        card5 = Card("Income tax refund. Collect $20.", 20)
-        card6 = Card("It's your birthday. Collect $10 from each player", 10)
-        card7 = Card("Life insurance matures. Collect $100", 100)
-        card8 = Card("Pay hospital fees of $100.", -100)
-        card9 = Card("Pay school fees of $50", -50)
-        card10 = Card("You have won a beauty contest. Collect $10", 10)
-        self.cChest.extend([card6, card2, card3, card4, card5, card1, card7,
-            card8, card9, card10])
-
-    def fillChanceDeck(self):
-        card1 = Card("Advance to Boardwalk", None)
-        card2 = Card("Advance to GO and collect $200", None)
-        card3 = Card("Advance to IL Ave.", None)
-        card4 = Card("Bank pays you dividend of $50", 50)
-        card5 = Card("Advance to St. Charles Place. If you pass go collect $200", None)
-        card6 = Card("Make general repairs on all your property, $25 for each house and $100 for each hotel",
-                None)
-        card7 = Card("Advance to Reading Railroad", None)
-        card8 = Card("Speeding fine $15", -15)
-        card9 = Card("You have been elected Chairman of the Board. Pay $50 to each player", -50)
-        card10 = Card("Your building loan matures. Collect $150", 150)
-        self.chance.extend([card1, card8, card3, card4, card6, card9, card2,
-            card7, card5, card10])
-
-    def pickACard(self):
-        #Picks the top card of the deck and then puts in at the bottom of the
-        #pile
-        if self.name == "Chance":
-            card = self.chance.pop(0)
-            self.chance.append(card)
-        else:
-            card = self.cChest.pop(0)
-            self.cChest.append(card)
-            print("cChest: ", self.cChest)
-        return card
+       
 
     def __repr__(self):
         return self.name
@@ -88,6 +44,7 @@ class Railroad(Block):
         #write a rent function
 
 class Tax(Block):
+    #Special block to handle luxury and income tax
     def __init__(self, location, color, name, price):
         super().__init__(location, color, name, price)
         self.property = None
@@ -98,11 +55,13 @@ class Tax(Block):
         return self.price
 
 class Utility(Block):
+    #Special class for the utility blocks
     def __init__(self, location, color, name, price):
         super().__init__(location, color, name, price)
         self.property = None
 
 class Corner(Block):
+    #Special block for the "corners" ie GO, Free Parking...
     def __init__(self, location, color, name, price, gTJ):
         super().__init__(location, color, name, price)
         self.property = None
